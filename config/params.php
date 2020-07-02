38dfd76b14d524c5f3d1336918475ffd509abc39
PuroCrack tu red social <latoprogramador@gmail.com>
2020-07-02 18:26:20 -0400

    guardar primeros cambios

---
+++ config/params.php
@@ -0,0 +1,6 @@
+<?php
+    return[
+    'adminEmail' => 'latoprogramador@gmail.com',
+    'title' => 'Ejemplo de Yii',
+    'salt' => 'fsddsflj38343lj0',
+];
---
+++ views/site/saluda.php
@@ -0,0 +1,14 @@
+<?= $mensaje ?>
+
+
+<?php
+    print("<br>");
+    foreach($array as $arr):
+
+        echo $arr;print("<br>");
+
+    endforeach;    
+
+
+
+?>
